4182cf60501b9ebe3135db0b4a14f7d01f931d83
aud19 <aud19980518@gmail.com>
2021-05-05 17:03:39 +0900

    명진test

--- src/main/java/com/example/jpetstore/JpetstoreBootApplication.java
+++ src/main/java/com/example/jpetstore/JpetstoreBootApplication.java
@@ -14,7 +14,7 @@ public class JpetstoreBootApplication {
 		return application.sources(JpetstoreBootApplication.class);
 	}
 	*/
-	//명진test
+	//명진test1
     public static void main(String[] args) throws Exception {
         SpringApplication.run(JpetstoreBootApplication.class, args);
     }
