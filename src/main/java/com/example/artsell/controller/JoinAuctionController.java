44e57d6d75ae4debae4d2c13611d6b5da767fe5a
aud19 <aud19980518@gmail.com>
2021-05-13 16:56:55 +0900

    addAuctionItem 메소드 추가

---
+++ src/main/java/com/example/artsell/controller/JoinAuctionController.java
@@ -0,0 +1,23 @@
+package com.example.artsell.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.jpetstore.service.ArtSellFacade;
+
+public class JoinAuctionController {
+	@Autowired
+	private ArtSellFacade artSell;
+	//입찰, 재입찰
+	private void addAuctionItem(@ModelAttribute("userSession") UserSession userSession, 
+			@RequestParam("ItemId") String workingItemId, 
+			@RequestParam("price") int price) throws Exception {
+		if (auctionItem.isNewUserPrice(userSession.getAccount().getUserId(), ItemId)) { //헌값 수정!
+				artSell.updatePrice(userId, itemId, price);
+		}
+		else {	//새로운 값
+			artSell.addPrice(userId, itemId, price);
+		}
+	}
+}
