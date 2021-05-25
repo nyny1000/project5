ba68c0e6b268defc991ded21db37410b1b23c869
nyny1000 <rnjsskdud941@naver.com>
2021-05-25 15:46:21 +0900

    옥션아이템맵퍼 푸시

--- src/main/java/com/example/artsell/dao/mybatis/mapper/AuctionItemMapper.java
+++ src/main/java/com/example/artsell/dao/mybatis/mapper/AuctionItemMapper.java
@@ -1,5 +1,6 @@
 package com.example.artsell.dao.mybatis.mapper;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -12,4 +13,24 @@ public interface AuctionItemMapper {
 	AuctionItem getAuctionItemByItemIdAndUserId(String itemId, String userId) throws DataAccessException;
 
 	Map<String, Integer> getBuyersByItemId(String itemId) throws DataAccessException;
+
+	List<AuctionItem> getAllAuctionItemByUserId(String userId) throws DataAccessException;
+
+	List<AuctionItem> getAuctionItem(String userId) throws DataAccessException;
+
+	int calcBestPrice(String itemId) throws DataAccessException;
+
+	void insertAuctionItem(AuctionItem auctionItem) throws DataAccessException;
+
+	void updatePrice(String userId) throws DataAccessException;
+
+	void deleteAuctionItem(String itemId) throws DataAccessException;
+
+	List<AuctionItem> getAuctionedItem(String userId) throws DataAccessException;
+
+	List<AuctionItem> getOrderedItem(String userId) throws DataAccessException;
+
+	int countAuctionJoinList(String userId) throws DataAccessException;
+
+	void changeState(String userId, String itemId) throws DataAccessException;
 }
