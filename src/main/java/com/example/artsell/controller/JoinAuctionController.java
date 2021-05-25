1182bb3ddf582c650996c639b8de4ceb72e603f0
aud19 <aud19980518@gmail.com>
2021-05-25 15:02:38 +0900

    mj3

--- src/main/java/com/example/artsell/controller/JoinAuctionController.java
+++ src/main/java/com/example/artsell/controller/JoinAuctionController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.artsell.domain.AuctionItem;
 import com.example.artsell.service.ArtSellFacade;
 
 @Controller
@@ -29,7 +30,7 @@ public class JoinAuctionController {
    public void addAuctionItem(@ModelAttribute("userSession") UserSession userSession, 
          @RequestParam("itemId") String itemId, 
          @RequestParam("price") int price) throws Exception {
-	  
+	  AuctionItem auctionItem;
 	  String userId = userSession.getAccount().getUserId();
       if (auctionItem.isNewUserPrice(userId, itemId)) { //헌값 수정!
             artSell.updatePrice(userId, itemId, price);
