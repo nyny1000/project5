a3f44dda7c2a30e16fe52f9c1d499d46d83550fb
nyny1000 <rnjsskdud941@naver.com>
2021-05-27 00:39:55 +0900

    옥션 전 커밋

--- src/main/java/com/example/artsell/controller/JoinAuctionController.java
+++ src/main/java/com/example/artsell/controller/JoinAuctionController.java
@@ -61,10 +61,7 @@ public class JoinAuctionController {
       //auctionitem table에서 해당 아이디 / 아이템아이디의 행 삭제
       artSell.deleteAuctionItem(userId, itemId);
    
-   
-   
       List<AuctionItem> auctionBuyerList = artSell.getBuyersByItemId(itemId);
-   
    
       if (auctionBuyerList != null) // 후순위자가 있다면
       {//후순위자에게 낙찰
