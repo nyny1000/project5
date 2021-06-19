242dc0c7e066debbc1f337dd1ae232de2ac9e55d
krlacodusk <krlacodusk@naver.com>
2021-06-19 20:24:33 +0900

    qq

--- src/main/java/com/example/artsell/controller/UserController.java
+++ src/main/java/com/example/artsell/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.example.artsell.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -19,96 +17,110 @@ import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.artsell.domain.Account;
-import com.example.artsell.domain.Item;
 import com.example.artsell.domain.AuctionItem;
 import com.example.artsell.service.AccountFormValidator;
 import com.example.artsell.service.AccountValidator;
 import com.example.artsell.service.ArtSellFacade;
 
 @Controller
-@SessionAttributes({"userSession","userList"})
+@SessionAttributes("userSession")
 public class UserController {
-   private ArtSellFacade artSell;
+	private ArtSellFacade artSell;
 
-   @Autowired
-   public void setArtSell(ArtSellFacade artSell) {
-      this.artSell = artSell;
-   }
+	@Autowired
+	public void setArtSell(ArtSellFacade artSell) {
+		this.artSell = artSell;
+	}
 
-   @RequestMapping("/loginform")
-   public String home(HttpServletRequest request) throws Exception {
-	   return "login";
-   }
+	@Autowired
+	private AccountValidator validator;
 
-   @RequestMapping("/user/login")
-   public ModelAndView login(HttpServletRequest request, @RequestParam("userId") String userId,
-         @RequestParam("password") String password,
-         @RequestParam(value = "forwardAction", required = false) String forwardAction, Model model)
-         throws Exception {
-      Account account = artSell.getAccount(userId, password);
-      if (account == null) {
-         return new ModelAndView("Error", "message", "Invalid username or password.  Signon failed.");
-      } else {
-         UserSession userSession = new UserSession(account);
-         model.addAttribute("userSession", userSession);
-         if (forwardAction != null) {
-            return new ModelAndView("redirect:" + forwardAction);
-         }
-         else
-            // return new ModelAndView("tiles/main"); // use Tiles
-            return new ModelAndView("redirect:/user/main"); //ny수정 redirect:/user/main
-      }
-   }
+	public void setValidator(AccountValidator validator) {
+		this.validator = validator;
+	}
 
-   @RequestMapping("/user/logout")
-   public String logout(HttpSession session, SessionStatus sessionStatus) throws Exception { //ny수정
-      session.removeAttribute("userSession");
-      session.invalidate();
-      sessionStatus.setComplete();
-      // return "tiles/login"; // use Tiles
-      return "redirect:/loginform"; //ny수정
-   }
+	@RequestMapping("/loginform")
+	public String home(HttpServletRequest request) throws Exception {
+		return "thyme/login";
+	}
 
-   @RequestMapping("/user/mypage")
-   public String myPage(@ModelAttribute("userSession") UserSession userSession) throws Exception {
-      // String userId = userSession.getAccount().getUserId();
-      return "myPage";
-   }
+	@ModelAttribute("loginAccount")
+	public Account formBackingObject(HttpServletRequest request) throws Exception {
+		System.out.println("로그인 폼백킹.");
+		return new Account();
 
-   // 2차수정
-   @RequestMapping("/user/delete")
-   public String deleteAccount(HttpSession session) throws Exception {
-      UserSession userSession = (UserSession) session.getAttribute("userSession");
-      String userId = userSession.getAccount().getUserId();
-      artSell.deleteAccount(userId);
+	}
 
-      session.removeAttribute("userSession");
-      session.invalidate();
-      
-      System.out.println("계정이 삭제되었습니다.");
-      return "main";
-   }
-   
-   @RequestMapping("/admin/manage")
-   public String getUserList(ModelMap model) {
-	   PagedListHolder<Account> userList = new PagedListHolder<Account>(this.artSell.getUserList());
-	   userList.setPageSize(20);
-	   model.put("userList", userList);
-	   return "userList";
-   }
-   
-   @RequestMapping("/admin/page")
-	public String handleRequest(
-			@RequestParam("page") String page, 
-			@ModelAttribute("userList") PagedListHolder<Account> userList,
-			BindingResult result) throws Exception {
+	@RequestMapping("/user/login")
+	public ModelAndView login(HttpServletRequest request, @ModelAttribute("loginAccount") Account loginAccount,
+			BindingResult result,
+			@RequestParam(value = "loginForwardAction", required = false) String loginForwardAction, Model model)
+			throws Exception {
+
+		System.out.println("로그인메소드로 넘어옴" + loginAccount);
+		validator.validate(loginAccount, result);
+
+		if (result.hasErrors()) {
+			return new ModelAndView("thyme/login");
+
+		} else {
+			Account account = artSell.getAccount(loginAccount.getUserId());
+			UserSession userSession = new UserSession(account);
+			model.addAttribute("userSession", userSession);
+			if (loginForwardAction != null) {
+				return new ModelAndView("redirect:" + loginForwardAction);
+			} else
+				return new ModelAndView("redirect:/user/main"); // ny수정 redirect:/user/main
+		}
+	}
+
+	@RequestMapping("/user/logout")
+	public String logout(HttpSession session, SessionStatus sessionStatus) throws Exception { // ny수정
+		session.removeAttribute("userSession");
+		session.invalidate();
+		sessionStatus.setComplete();
+		// return "tiles/login"; // use Tiles
+		return "redirect:/loginform"; // ny수정
+	}
+
+	@RequestMapping("/user/mypage")
+	public String myPage(@ModelAttribute("userSession") UserSession userSession) throws Exception {
+		// String userId = userSession.getAccount().getUserId();
+		return "myPage";
+	}
+
+	// 2차수정
+	@RequestMapping("/user/delete")
+	public String deleteAccount(HttpSession session) throws Exception {
+		UserSession userSession = (UserSession) session.getAttribute("userSession");
+		String userId = userSession.getAccount().getUserId();
+		artSell.deleteAccount(userId);
+
+		session.removeAttribute("userSession");
+		session.invalidate();
+
+		System.out.println("계정이 삭제되었습니다.");
+		return "main";
+	}
+
+	@RequestMapping("/admin/manage")
+	public String getUserList(ModelMap model) {
+		PagedListHolder<Account> userList = new PagedListHolder<Account>(this.artSell.getUserList());
+		userList.setPageSize(20);
+		model.put("userList", userList);
+		return "userList";
+	}
+
+	@RequestMapping("/admin/page")
+	public String handleRequest(@RequestParam("page") String page,
+			@ModelAttribute("userList") PagedListHolder<Account> userList, BindingResult result) throws Exception {
 
 		if ("next".equals(page)) {
 			userList.nextPage();
 		} else if ("previous".equals(page)) {
 			userList.previousPage();
-		} 
-		
+		}
+
 		return "userList";
 	}
 
