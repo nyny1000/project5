7c0e236a08ee3358644221ec80dce7800fb3d1bb
kyounje <kyounje@naver.com>
2021-06-21 11:31:45 +0900

    ㅇ

--- src/main/java/com/example/artsell/WebMvcConfig.java
+++ src/main/java/com/example/artsell/WebMvcConfig.java
@@ -31,12 +31,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override 
 	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(loginInterceptor)
-//				.addPathPatterns("/home", "/user/delete", "/user/update", "/user/mypage", "/search/*", "/interesting/*", "/myitem/*", "/auction/*", "/shop/*"); 
-//		registry.addInterceptor(auctionInterceptor)
-//				.addPathPatterns("/auction/info", "/auction/bid");
-		registry.addInterceptor(logoutInterceptor)
-				.addPathPatterns("/*");
+		registry.addInterceptor(loginInterceptor)
+				.addPathPatterns("/home", "/user/delete", "/user/update", "/user/mypage", "/search/*", "/interesting/*", "/myitem/*", "/auction/*", "/shop/*"); 
+		registry.addInterceptor(auctionInterceptor)
+				.addPathPatterns("/auction/info", "/auction/bid");
 	}
 	  
 	  
